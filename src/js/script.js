4a23354dd8465da9a4b795408f936421327948ad
Sergey Karakoskin <karakoskinsergej@gmail.com>
2025-08-15 19:06:45 +0300

    feat/develop - pages: add other pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
+import JustValidate from 'just-validate';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -48,3 +50,147 @@ try {
 } catch (e) {
     console.error('slider selector not found');
 }
+
+try {
+    const tabs = document.querySelectorAll('.catalog__tab');
+    const contents = document.querySelectorAll('.catalog__content-item');
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            // Удаляем активный класс у всех табов и контента
+            tabs.forEach((t) => t.classList.remove('catalog__tab_active'));
+            contents.forEach((c) => (c.style.display = 'none'));
+
+            // Добавляем активный класс к нажатому табу и показываем соответствующий контент
+            tab.classList.add('catalog__tab_active');
+            contents[index].style.display = 'flex';
+        });
+    });
+
+    // Показываем первый контент при загрузке
+    contents.forEach((c, i) => (c.style.display = i === 0 ? 'flex' : 'none'));
+} catch (e) {
+    console.error('tabs selector not found');
+}
+
+try {
+    const validatorTouch = new JustValidate('.form');
+
+    validatorTouch
+        .addField('#name', [
+            {
+                rule: 'required',
+                errorMessage: 'Please fill the name',
+            },
+            {
+                rule: 'minLength',
+                value: 2,
+                errorMessage: 'Minimum 2 chars!',
+            },
+        ])
+        .addField('#email', [
+            {
+                rule: 'required',
+            },
+            {
+                rule: 'email',
+            },
+        ])
+        .addField(
+            '#question',
+            [
+                {
+                    rule: 'required',
+                },
+                {
+                    rule: 'minLength',
+                    value: 5,
+                },
+            ],
+            {
+                errorsContainer: document
+                    .querySelector('#question')
+                    .parentElement.querySelector('.error-message'),
+            }
+        )
+        .addField(
+            '#checkbox',
+            [
+                {
+                    rule: 'required',
+                },
+            ],
+            {
+                errorsContainer: document
+                    .querySelector('#checkbox')
+                    .parentElement.parentElement.querySelector(
+                        '.checkbox-error-message'
+                    ),
+            }
+        )
+        .onSuccess((event) => {
+            const form = event.currentTarget;
+            const formData = new FormData(form);
+
+            fetch('https://httpbin.org/post', {
+                method: 'POST',
+                body: formData,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log('Success', data);
+                    form.reset();
+                });
+        });
+} catch (e) {}
+
+try {
+    const validatorFooter = new JustValidate('.footer__form');
+
+    validatorFooter
+        .addField(
+            '#footer__email',
+            [
+                {
+                    rule: 'required',
+                },
+                {
+                    rule: 'email',
+                },
+            ],
+            {
+                errorsContainer: document
+                    .querySelector('#footer__email')
+                    .parentElement.querySelector('.email-error-message'),
+            }
+        )
+        .addField(
+            '#footer__checkbox',
+            [
+                {
+                    rule: 'required',
+                },
+            ],
+            {
+                errorsContainer: document
+                    .querySelector('#footer__checkbox')
+                    .parentElement.parentElement.querySelector(
+                        '.check-error-message'
+                    ),
+            }
+        )
+        .onSuccess((event) => {
+            const form = event.currentTarget;
+            const formData = new FormData(form);
+
+            fetch('https://httpbin.org/post', {
+                method: 'POST',
+                body: formData,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log('Success', data);
+                    form.reset();
+                });
+        });
+} catch (e) {}
